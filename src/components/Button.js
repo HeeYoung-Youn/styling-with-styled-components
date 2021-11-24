72d3d4184806b0ec73c0d88dc6e189aac9d8e05e
HeeYoung-Youn <yhy1@naver.com>
2021-11-24 17:08:48 +0900

    Add: Styled-components - Polished
    Polished 스타일 유틸 사용해서 lighten, darken 처리 / 색상 styled-components 외부에서 별도로 관리

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,27 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { darken, lighten } from "polished";
+
+const colorStyles = css`
+  /* 색상 */
+  ${({ theme, color }) => {
+    const selected = theme.palette[color];
+    return css`
+      background: ${selected};
+      &:hover {
+        background: ${lighten(0.1, selected)};
+      }
+      &:active {
+        background: ${darken(0.1, selected)};
+      }
+    `;
+  }}
+`;
 
 const StyledButton = styled.button`
   /* 공통 스타일 */
   display: inline-flex;
+  align-items: center;
   outline: none;
   border: none;
   border-radius: 4px;
@@ -12,29 +30,28 @@ const StyledButton = styled.button`
   cursor: pointer;
   padding-left: 1rem;
   padding-right: 1rem;
-  align-items: center;
 
   /* 크기 */
   height: 2.25rem;
   font-size: 1rem;
 
-  /* 색상 */
-  background: #228be6;
-  &:hover {
-    background: #339af0;
-  }
-  &:active {
-    background: #1c7ed6;
-  }
-
+  ${colorStyles}
   /* 기타 */
   & + & {
     margin-left: 1rem;
   }
 `;
 
-function Button({ children, ...rest }) {
-  return <StyledButton {...rest}>{children}</StyledButton>;
+function Button({ children, color, ...rest }) {
+  return (
+    <StyledButton color={color} {...rest}>
+      {children}
+    </StyledButton>
+  );
 }
+
+Button.defaultProps = {
+  color: "blue",
+};
 
 export default Button;
